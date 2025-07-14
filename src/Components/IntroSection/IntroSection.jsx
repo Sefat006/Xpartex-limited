553fa79dea6f94284d8e8979a22379b91dde08da
sefat006 <sefatur76@gmail.com>
2025-07-14 19:07:19 +0600

    right created

--- src/Components/IntroSection/IntroSection.jsx
+++ src/Components/IntroSection/IntroSection.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import EpicDesign from './EpicDesign';
 import WeCreateWebsite from './WeCreateWebsite';
-import rightSection from './rightSection';
+import Right from './Right';
 
 const IntroSection = () => {
     return (
         <div className='bg-[#D8C9BB] space-y-20 p-10 text-black'>
-            <rightSection></rightSection>
+            <Right></Right>
             <WeCreateWebsite></WeCreateWebsite>
             <EpicDesign></EpicDesign>
         </div>
---
+++ src/Components/IntroSection/Right.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Right = () => {
+    return (
+        <div>
+            <div className='bg-yellow-400 h-50'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Right;
